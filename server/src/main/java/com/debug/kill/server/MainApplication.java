6d57a1b7198f83c83b988b24abdcc107c65b23db
zhengzekun <37758532+zhengzekun@users.noreply.github.com>
2020-02-06 21:59:56 +0800

    The first transportable version
    The first transportable version

---
+++ server/src/main/java/com/debug/kill/server/MainApplication.java
@@ -0,0 +1,25 @@
+package com.debug.kill.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+//操作数据库
+@ImportResource(value = {"classpath:spring/spring-jdbc.xml"})
+//mybatis
+@MapperScan(basePackages = "com.debug.kill.model.mapper")
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
